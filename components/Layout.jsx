581bea99e86b5f6aadda6198abaa4a0c99e5556b
quochuydev <quochuy.dev@gmail.com>
2021-08-28 13:29:27 +0700

    update()

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 import Header from "./Header";
@@ -11,17 +11,18 @@ import Contact from "./Contact";
 import Newsletter from "./Newsletter";
 import Footer from "./Footer";
 
-export default function Layout({ products }){
-  
-  return <div>
-    <Header />
-    <Home />
-    <Banner />
-    <Category />
-    <Products products={products}/>
-    <Deal />
-    <Contact />
-    <Newsletter />
-    <Footer />
-  </div>
+export default function Layout({ products }) {
+  return (
+    <div>
+      <Header />
+      <Home />
+      <Banner />
+      <Category />
+      <Products products={products} />
+      <Deal />
+      <Contact />
+      <Newsletter />
+      <Footer />
+    </div>
+  );
 }
--- components/SearchBox.jsx
+++ components/SearchBox.jsx
@@ -1,6 +1,24 @@
-export default function SearchBox(){
-    return <form className="search-box-container">
-    <input type="search" id="search-box" placeholder="search here..." />
-    <label htmlFor="search-box" className="fas fa-search" />
-  </form>
+import React, { useState } from "react";
+
+export default function SearchBox() {
+  const [keyword, setKeyword] = useState("");
+
+  const search = (q) => {
+    return alert("seach: " + q);
+  };
+
+  return (
+    <form className="search-box-container">
+      <input
+        type="text"
+        name="q"
+        id="search-box"
+        placeholder="search here..."
+        onChange={(e) => setKeyword(e.target.value)}
+      />
+      <a href="#" onClick={() => search(keyword)}>
+        <label htmlFor="search-box" className="fas fa-search" />
+      </a>
+    </form>
+  );
 }
