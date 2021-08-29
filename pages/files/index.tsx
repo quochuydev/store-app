aa525883251a552ded2e18aa6271e63ff145bb40
quochuydev <quochuy.dev@gmail.com>
2021-08-29 11:59:04 +0700

    update()

---
+++ pages/files/index.tsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function Files() {
+  const [image, setImage] = useState(null);
+  const [files, setFiles] = useState([]);
+
+  const fetchFiles = () => {
+    axios.get(process.env.SERVER_URL + "/api/files").then((result) => {
+      setFiles(result?.data?.items || []);
+    });
+  };
+
+  useEffect(() => {
+    fetchFiles();
+  }, []);
+
+  const onCreateProduct = async () => {
+    await axios.post(process.env.SERVER_URL + "/api/products", {
+      image,
+    });
+    alert("success");
+  };
+
+  const onCopy = (text: string) => {
+    navigator.clipboard.writeText(text).then(
+      function () {
+        console.log("Async: Copying to clipboard was successful!");
+      },
+      function (err) {
+        console.error("Async: Could not copy text: ", err);
+      }
+    );
+  };
+
+  return (
+    <>
+      <button onClick={onCreateProduct}>create product</button>
+
+      <input
+        type="file"
+        accept="image/*"
+        onChange={async (event: any) => {
+          try {
+            const file = event.target?.files[0];
+            console.log(file);
+            const bodyFormData = new FormData();
+            bodyFormData.append("files", file);
+
+            const result = await axios({
+              method: "post",
+              url: process.env.SERVER_URL + "/api/files",
+              headers: { "Content-Type": "multipart/form-data" },
+              data: bodyFormData,
+            });
+            setImage(result?.data?.url);
+          } catch (error) {
+            //
+          }
+          fetchFiles();
+        }}
+      />
+
+      {files.map((e: any, i: number) => (
+        <ul key={i}>
+          <li>{e._id}</li>
+          <li onClick={() => onCopy(e.url)}>{e.url}</li>
+        </ul>
+      ))}
+    </>
+  );
+}
