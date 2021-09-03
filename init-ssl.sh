73bc5965085093c25ff60ad7776adea44d08a358
quochuydev <quochuy.dev@gmail.com>
2021-09-03 14:11:30 +0700

    update() nginx

--- init-ssl.sh
+++ init-ssl.sh
@@ -7,7 +7,7 @@ fi
 
 domains=(cafeman.xyz)
 rsa_key_size=4096
-data_path="./data/certbot"
+data_path="/data/certbot"
 email="quochuydev1@gmail.com" # Adding a valid address is strongly recommended
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
