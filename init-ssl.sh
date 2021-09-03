9476368eb31650f1d9ffa35eeb2ebd155db03a86
quochuydev <quochuy.dev@gmail.com>
2021-09-03 14:01:25 +0700

    update() nginx

--- init-ssl.sh
+++ init-ssl.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(example.xyz)
+domains=(cafeman.xyz)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="huy.pham@inapps.net" # Adding a valid address is strongly recommended
