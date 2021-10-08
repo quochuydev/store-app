e4076ef1b4f9e1bc4732a301ca8be7a89c3a71aa
quochuydev <quochuy.dev@gmail.com>
2021-10-08 13:23:44 +0700

    update()

--- commands/init-ssl-service.sh
+++ commands/init-ssl-service.sh
@@ -8,7 +8,7 @@ fi
 domains=(service.cafeman.xyz)
 rsa_key_size=4096
 data_path="/data/certbot"
-email="quochuydev1@gmail.com" # Adding a valid address is strongly recommended
+email="cafeman@yopmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
