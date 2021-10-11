73eb7f956ffbff5c5982d227e965f77ea7bf3d22
quochuydev <quochuy.dev@gmail.com>
2021-10-11 21:39:06 +0700

    update() cron

--- server/index.js
+++ server/index.js
@@ -7,6 +7,8 @@ const { v4: uuidv4 } = require("uuid");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
+const cron = require("node-cron");
+
 const port = 3000;
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
@@ -18,6 +20,8 @@ const { productRoute } = require("./routes/product");
 const { orderRoute } = require("./routes/order");
 const { coreRoute } = require("./routes/core");
 const { settingRoute } = require("./routes/setting");
+
+const { productModel } = require("./models/product");
 
 console.log("*********************************");
 console.log("port:", port);
@@ -33,6 +37,17 @@ mongoose.connect(process.env.DATABASE_URL, {
 
 app.prepare().then(() => {
   const server = express();
+
+  cron.schedule("0 0 * * * *", async () => {
+    console.log(new Date(), "create every hour", process.env.MESSAGE);
+
+    await productModel.create({
+      title: `Cà phê Mê Trang Arabica (A) - Hộp 250g rang xay hút chân không`,
+      description: process.env.MESSAGE,
+      image: "https://cf.shopee.vn/file/76698f5a72286c1f9aca14956b11d0db",
+      price: 74000,
+    });
+  });
 
   server.use(cors({ credentials: true, origin: true }));
   server.use(bodyParser.json({}));
