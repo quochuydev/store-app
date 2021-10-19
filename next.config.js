28c1f2212cc115cca7b6679ecc5fc97c645c1972
quochuydev <quochuy.dev@gmail.com>
2021-10-19 17:17:36 +0700

    update

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    NODE_ENV: process.env.NODE_ENV,
     SERVER_URL: process.env.SERVER_URL,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID,
