d0da29ca9808406422ea363b9b81654dead25ca3
quochuydev <quochuy.dev@gmail.com>
2021-10-19 19:18:52 +0700

    update

--- commands/build.sh
+++ commands/build.sh
@@ -1,7 +1,4 @@
 sudo git reset --hard
 sudo git pull
-# sudo docker-compose build
-# sudo docker login
-# sudo docker tag quochuydev/store-app quochuydev/store-app:1.0.11
-# sudo docker push quochuydev/store-app:1.0.11
+sudo docker-compose build
 sudo docker-compose up -d
