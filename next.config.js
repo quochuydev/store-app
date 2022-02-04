056fd53376de071ed1d487fc51529a4482833590
quochuydev <quochuy.dev@gmail.com>
2022-02-04 22:59:16 +0700

    update

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    SERVER_URL: process.env.SERVER_URL,
+    SERVER_URL: process.env.SERVER_URL || 'https://store-app-seven.vercel.app',
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID,
   },
