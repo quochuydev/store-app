8b49b5535c4b6911a4e8ad3d90012cefbaeaa7e6
quochuydev <quochuy.dev@gmail.com>
2022-03-06 00:49:39 +0700

    update

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -10,7 +10,7 @@ export default function Menu() {
 
   return (
     <nav className="navbar">
-      <Link href="/#home">home</Link>
+      <Link href="/#home">{t("navigation.home")}</Link>
       <Link href="/#category">{t("navigation.category")}</Link>
       {/* <MegaMenu /> */}
       <Link href="/products">{t("navigation.product")}</Link>
