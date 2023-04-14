d7e5363d3f2370e7eafe9b1d71dee1242009766b
quochuydev <quochuy.dev@gmail.com>
2023-04-15 06:28:25 +0700

    update

--- components/Product/ProductDetail/index.jsx
+++ components/Product/ProductDetail/index.jsx
@@ -297,7 +297,7 @@ export default function ProductDetail({ product, after }) {
                 type="submit"
                 onClick={addToCart}
                 disabled={loading}
-                className="mt-10 w-full bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                className="text-white bg-indigo-600 mt-10 w-full border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
                 Add to cart
               </button>
